cbfc1cbfe9c98e890e4f48a16b8a23b6f3833e90
Volodya <chuchman002@gmail.com>
2023-10-11 16:58:29 +0300

    Add project files.

---
+++ LAB №4.3/LAB №4.3.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+double F(double x, double a, double b, double c) {
+    if (x < 0 && b != 0)
+        return a * pow(x, 2) + pow(b, 2) * x;
+    else 
+        if (x > 0 && b == 0)
+        return (x + a) / (x + c);
+    else
+        return (x / c);
+}
+
+int main() {
+    double x, xp, xk, dx, a = 0, b = 0, c = 1;
+    cout << "xp = "; cin >> xp;
+    cout << "xk = "; cin >> xk;
+    cout << "dx = "; cin >> dx;
+    cout << fixed;
+    cout << "---------------------------" << endl;
+    cout << "|" << setw(5) << "x" << " |"
+        << setw(7) << "F" << " |" << endl;
+    cout << "---------------------------" << endl;
+    x = xp;
+    while (x <= xk) {
+        double f = F(x, a, b, c);
+        cout << "|" << setw(7) << setprecision(2) << x
+            << " |" << setw(10) << setprecision(3) << f
+            << " |" << endl;
+        x += dx;
+    }
+    cout << "---------------------------" << endl;
+    return 0;
+}
